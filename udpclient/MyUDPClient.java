0d456336a2f64f3750b4c0149c240740f69f43de
William James <james133@mail.chapman.edu>
2018-06-08 21:36:58 -0700

    Add files via upload

---
+++ udpclient/MyUDPClient.java
@@ -0,0 +1,52 @@
+/**
+*	UDP Client Program
+*	Connects to a UDP Server
+*	Receives a line of input from the keyboard and sends it to the server
+*	Receives a response from the server and displays it.
+*
+*	@author: William James
+@	version: 2.1
+*/
+
+import java.io.*;
+import java.net.*;
+
+class MyUDPClient {
+    public static void main(String args[]) throws Exception
+    {
+
+      BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
+
+      DatagramSocket clientSocket = new DatagramSocket();
+
+      InetAddress IPAddress = InetAddress.getByName("localhost");
+
+      byte[] sendData = new byte[1024];
+      byte[] receiveData = new byte[1024];
+
+
+      while(true){
+        String sentence = inFromUser.readLine();
+        if(sentence.equals("Goodbye")){
+          break;
+        }
+        sendData = sentence.getBytes();
+
+  	    DatagramPacket sendPacket = new DatagramPacket(sendData, sendData.length, IPAddress, 9876);
+
+        clientSocket.send(sendPacket);
+
+        DatagramPacket receivePacket = new DatagramPacket(receiveData, receiveData.length);
+
+        clientSocket.receive(receivePacket);
+
+        String modifiedSentence = new String(receivePacket.getData());
+
+
+        System.out.println("FROM SERVER:" + modifiedSentence);
+
+      }
+        clientSocket.close();
+
+      }
+}
